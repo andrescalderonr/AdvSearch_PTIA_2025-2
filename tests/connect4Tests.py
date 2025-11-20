c7e5483e0741556887296a843573ef39109d6885
Andres <andres.calderon-r@mail.escuelaing.edu.co>
2025-11-20 13:38:18 -0500

    Part 1 development

---
+++ tests/connect4Tests.py
@@ -0,0 +1,14 @@
+from gameImplementation import connect4
+
+game = connect4.start()
+
+print("Estado inicial:")
+print(game.toString())
+
+# Obtener movimientos posibles
+next_states = game.actionResults()
+print("Cantidad de movimientos posibles:", len(next_states))
+
+# Mostrar un movimiento de ejemplo
+print("\nPrimer movimiento posible:")
+print(next_states[0].toString())
